227293848a6b7f6a0cd215bf61c0b20663f91144
Gabriel <prismaticnetwork12@gmail.com>
2021-10-31 19:33:40 -0300

    feat: homepage / sign in form almost done and bulma framework added to project

---
+++ frontend/saturnssolutionsfrontend/src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <span>Hello world</span>
+  </div>
+</template>
+
+<script>
+export default {
+  title: 'TakeMoney - Login',
+  created() {
+    document.title = 'TakeMoney - Login';
+  }
+}
+</script>
+
+<style>
+
+</style>
